6aa61aeea75552b0da0f602d4afb6cd77b11cd59
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-03-06 10:19:54 -0500

    edit for others to input their own keys

--- server/routes/keys.js
+++ server/routes/keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-    consumer_key: '90DUAjdSUx35WJ49lnZXneebG',
-    consumer_secret: 'ouYYtz2b4sP61yHQAPAVg8E8FoGsNYxE5picLPNrs5xajUXiKg',
-    access_token_key: '230944272-YHZ9ixbmgV8F0wqyJP5seEMnnM9T7ARytSnBWyGa',
-    access_token_secret: 'grPltr64BV8lxvAlc4y7V1oFvSvxU42TIni2KJgD7SYr4'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 };
